072af9fd671ce6e65e7310358bd71fdef617999f
Two <njsolomonoh@gmail.com>
2022-03-02 15:14:36 -0500

    Add files via upload

--- src/ws/message.js
+++ src/ws/message.js
@@ -6,6 +6,7 @@ module.exports = (ws, message, isBinary) => {
 
     const content = enc.decode(new Uint8Array(message));
     if (!content.length) return;
+    if (content.length > 2048) return;
 
     const send = JSON.stringify([ ws.username, content ]);
     for (let connection of connections) connection.send(send);
