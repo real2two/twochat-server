4724801c829fae587c74a26661e7e6548bb40fd1
Two <njsolomonoh@gmail.com>
2022-03-17 23:23:46 -0400

    Update close.js

--- src/ws/close.js
+++ src/ws/close.js
@@ -1,9 +1,9 @@
-let connections = require("./connections");
+const connections = require("./connections");
 
 module.exports = async ws => {
     connections.splice(connections.indexOf(ws), 1);
 
     // Sends leave message.
     const content = JSON.stringify({ a: 1, u: ws.username });
-    for (let connection of connections) connection.send(content);
+    for (const connection of connections) connection.send(content);
 }
